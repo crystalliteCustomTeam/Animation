a6f75a4081eb668a161ca60b7ba435fa69368935
alim-2023 <alim@crystallitedigital.com>
2024-02-13 22:34:00 +0500

    rev.5

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,7 +34,7 @@ export default function RootLayout({ children }) {
 
 
 {/* Meta Pixel Code */}
-
+{/* 
             <Script>
                 {`
                    !function(f,b,e,v,n,t,s)
@@ -48,7 +48,7 @@ export default function RootLayout({ children }) {
                    fbq('init', '403846962177452');
                    fbq('track', 'PageView');
                 `}
-            </Script>
+            </Script> */}
 
 
 
