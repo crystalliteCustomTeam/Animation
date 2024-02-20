2f6c4808f2d73a12c34fb1f590b6ecff0c8dd854
dev-sikander <sikander@crystallitedigital.com>
2024-02-21 01:26:06 +0500

    Thumbnails and videos are done in Tabs Section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,16 +1,45 @@
 "use client"
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 // Import Css
 import styles from "./PortFolio.module.css"
+import Link from 'next/link';
 
+import { Fancybox as NativeFancybox } from "@fancyapps/ui"
+import "@fancyapps/ui/dist/fancybox/fancybox.css"
+import Image from 'next/image';
+
+// icons
+import PlayIcon from "media/icons/video-icon.png"
 
 const Portfolio = ({ content }) => {
-    const { title, para, tabInfo, tabContents, tabsInfo } = content;
+    const { title, para, tabInfo, tabContents, tabsInfo, tabThumbnail } = content;
+    console.log(tabContents)
     //Tabs
     const [activeTab, setActiveTab] = useState(0);
     const handleTabClick = (index) => {
         setActiveTab(index);
     };
+
+    // fancy box 
+    function Fancybox(props) {
+        const containerRef = useRef(null);
+
+        useEffect(() => {
+            const container = containerRef.current;
+
+            const delegate = props.delegate || "[datafancybox]";
+            const options = props.options || {};
+
+            NativeFancybox.bind(container, delegate, options);
+
+            return () => {
+                NativeFancybox.unbind(container);
+                NativeFancybox.close();
+            };
+        });
+
+        return <div ref={containerRef}>{props.children}</div>;
+    }
     return (
         <>
             <section className={`w-full flex items-center justify-start py-6 md:py-8 lg:py-16`}>
@@ -25,26 +54,36 @@ const Portfolio = ({ content }) => {
                             </p>
                         </div>
                     </div>
-                    <div className={`${tabsInfo ? "mt-8 md:mt-12" : "mt-0"}`}>
-                        {tabsInfo ?
-                            <ul className="flex flex-wrap justify-center text-sm font-medium text-center gap-3 md:gap-5 lg:w-11/12 mx-auto">
-                                {tabInfo.map((tab, index) => (
-                                    <li key={index}
-                                        className={`inline-block w-[180px] xl:w-[220px] py-3 text-[16px] lg:text-[20px] font-normal font-sans text-white border rounded-[27px] cursor-pointer hover:border-primary-100 ${activeTab === index ? 'border-primary-100' : 'border-[#646464]'}`}
-                                        onClick={() => handleTabClick(index)}>
-                                        {tab.label}
-                                    </li>
-                                ))}
-                            </ul> : null}
+                    <div className={`"mt-8 md:mt-12`}>
+                        <ul className="flex flex-wrap justify-center text-sm font-medium text-center gap-3 md:gap-5 lg:w-11/12 mx-auto">
+                            {tabInfo.map((tab, index) => (
+                                <li key={index}
+                                    className={`inline-block w-[180px] xl:w-[220px] py-3 text-[16px] lg:text-[20px] font-normal font-sans text-white border rounded-[27px] cursor-pointer hover:border-primary-100 ${activeTab === index ? 'border-primary-100' : 'border-[#646464]'}`}
+                                    onClick={() => handleTabClick(index)}>
+                                    {tab.label}
+                                </li>
+                            ))}
+                        </ul>
                         <div className="tabs-content pt-7 md:pt-12">
                             {tabContents[activeTab] && (
-                                <div className='grid grid-cols-3 gap-3 md:gap-8'>
-                                    {tabContents[activeTab].map((item, index) => (
-                                        <div key={index}>
-                                            <video className={`${styles.shadow} w-full h-full`} muted="muted" autoPlay src={item}></video>
-                                        </div>
-                                    ))}
-                                </div>
+                                <Fancybox options={{
+                                    Carousel: {
+                                        infinite: false,
+                                    },
+                                }}>
+                                    <div className='grid grid-cols-3 gap-3 md:gap-8'>
+                                        {tabContents[activeTab].map((item, index) => (
+                                            <Link key={index} href={item.video} datafancybox="gallery" className={`${styles.shadow} w-full h-full group`}>
+                                                <div className="overlay relative">
+                                                    <Image src={item.thumbnail} alt="Infinity Animations" width={468} height={263} className='min-h-[263px]' />
+                                                    <div className="bg-secondary-100/70 h-full w-full absolute left-[50%] top-[50%] translate-x-[-50%] group-hover:translate-y-[-132px] translate-y-[132px] flex items-center justify-center ">
+                                                        <Image src={PlayIcon} alt='Play-icon' className='brightness-200 invert-0' />
+                                                    </div>
+                                                </div>
+                                            </Link>
+                                        ))}
+                                    </div>
+                                </Fancybox>
                             )}
                         </div>
                     </div>
