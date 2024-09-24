d855c3676c83d5d8b58a98c126c86af9711f5d8d
dev-sikander <sikandar@crystallitedigital.com>
2024-09-24 23:44:47 +0500

    merge with murtaza

--- src/app/animation-studio-atlanta/layout.js
+++ src/app/animation-studio-atlanta/layout.js
@@ -2,11 +2,11 @@
 export const metadata = {
     //===== Meta Tags =====
     title: "Top Animation Studio in Atlanta - Infinity Animations",
-    description: "Infinity Animations is an animation studio in atlanta specializing in all types of animation services. We create explainer videos, demo videos, and all other types of videos. Contact us today.",
+    description: "Infinity Animations is an animation studio in Atlanta specializing in all types of animation services. We create explainer videos, demo videos, and all other types of videos. Contact us today.",
     //===== OG Tags =====
     openGraph: {
         title: 'Top Animation Studio in Atlanta - Infinity Animations',
-        description: 'Infinity Animations is an animation studio in atlanta specializing in all types of animation services. We create explainer videos, demo videos, and all other types of videos. Contact us today.',
+        description: 'Infinity Animations is an animation studio in Atlanta specializing in all types of animation services. We create explainer videos, demo videos, and all other types of videos. Contact us today.',
         url: '/animation-studio-atlanta',
         siteName: 'Infinity Animations',
         locale: 'en_US',
