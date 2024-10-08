ec8db7190b79b1150d8b9619bce3483dd97c7381
dev-sikander <sikandar@crystallitedigital.com>
2024-10-08 05:40:34 +0500

    sections import dynemacilly

---
+++ src/app/game-lp/components/OurProcess/OurProcess.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const OurProcess = () => {
+    return (
+        <section>
+            <div className="max-w-7xl mx-auto">
+                <div className="grid grid-cols-12">
+                    <div className="col-span-12">
+                        <div className="txt">
+                            <h2 className='text-[30px] sm:text-[50px] lg:text-[60px] leading-[70px] sm:leading-[80px] lg:leading-[90px] font-semibold text-white poppins md:mb-3 mb-4 lg:mb-10'>
+                                Our Process
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default OurProcess
--- src/app/game-lp/page.js
+++ src/app/game-lp/page.js
@@ -1,12 +1,5 @@
 import React from "react";
-import Header from "./components/Header/Header";
-import Banner from "./components/Banner/Banner";
-import Clients from "./components/Clients/Clients";
-import TwoDArt from "./components/2dArt/2dArt";
-import ThreeDArt from "./components/3dArt/3dArt";
-import VFX from "./components/VFX/VFX";
-import Portfolio from "./components/Portfolio/Portfolio";
-import SmallBanner from "./components/SmallBanner/SmallBanner";
+import { Banner, Clients, Header, OurProcess, Portfolio, SmallBanner, ThreeDArt, TwoDArt, VFX, WhyChoose } from "./components";
 
 export default function Page() {
     return (
@@ -19,6 +12,8 @@ export default function Page() {
             <VFX />
             <Portfolio />
             <SmallBanner />
+            <WhyChoose />
+            <OurProcess />
         </>
     )
 }
