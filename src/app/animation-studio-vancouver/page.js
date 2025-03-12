1231a938948d586cc7c18c918b3f101c85e6dab8
Ahmed-Crystallite3 <ahmed.r@crystallitedigital.com>
2025-03-12 21:42:00 +0500

    optimization

--- src/app/animation-studio-vancouver/page.js
+++ src/app/animation-studio-vancouver/page.js
@@ -599,8 +599,8 @@ const LocationPage = () => {
     <>
       <div className="bg-black">
         <Hero content={hero} />
-        {/* <Description content={Desc1} />
-        <Portfolio content={portfolio} />
+        <Description content={Desc1} />
+        {/* <Portfolio content={portfolio} />
         <Process content={processContent} />
         <Why content={why2} />
         <Pricing content={pricing} />
