f00aa4bfffe1ef9d118e4709b0a9782a9d668ee5
Ahmed-Crystallite3 <ahmed.r@crystallitedigital.com>
2025-04-09 21:59:39 +0500

    3 new location pages updated

---
+++ src/app/animation-studio-san-francisco/layout.js
@@ -0,0 +1,25 @@
+//===== Meta Data =====
+export const metadata = {
+  //===== Meta Tags =====
+  title: "Best Animation Studio in San Francisco - Infinity Animations",
+  description:
+    "Discover top-rated animation studios in San Francisco. We create compelling 2D, 3D, motion graphics, and stop motion animation that connects and converts. Let’s bring your vision to life",
+  //===== OG Tags =====
+  openGraph: {
+    title: "Best Animation Studio in San Francisco - Infinity Animations",
+    description:
+      "Discover top-rated animation studios in San Francisco. We create compelling 2D, 3D, motion graphics, and stop motion animation that connects and converts. Let’s bring your vision to life",
+    url: "/animation-studio-san-francisco",
+    siteName: "Infinity Animations",
+    locale: "en_US",
+    type: "website",
+  },
+  //===== Canonical =====
+  alternates: { canonical: "/animation-studio-san-francisco" },
+  //===== GEO Tags =====
+  other: {},
+}
+
+export default function RootLayout({ children }) {
+  return children
+}
---
+++ src/app/animation-studio-seattle/layout.js
@@ -0,0 +1,25 @@
+//===== Meta Data =====
+export const metadata = {
+  //===== Meta Tags =====
+  title: "Best Animation Studio in Seattle - Infinity Animations",
+  description:
+    "Looking for top animation studios in Seattle? Our Seattle animation studio offers expert 2D & 3D animation, motion graphics, and whiteboard animation. Contact us today.",
+  //===== OG Tags =====
+  openGraph: {
+    title: "Best Animation Studio in Seattle - Infinity Animations",
+    description:
+      "Looking for top animation studios in Seattle? Our Seattle animation studio offers expert 2D & 3D animation, motion graphics, and whiteboard animation. Contact us today.",
+    url: "/animation-studio-seattle",
+    siteName: "Infinity Animations",
+    locale: "en_US",
+    type: "website",
+  },
+  //===== Canonical =====
+  alternates: { canonical: "/animation-studio-seattle" },
+  //===== GEO Tags =====
+  other: {},
+}
+
+export default function RootLayout({ children }) {
+  return children
+}
