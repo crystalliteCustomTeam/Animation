5145d6a6043f6949952c9ad7a5a4af339895ebf0
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2025-07-08 23:13:44 +0500

    new page folsom

---
+++ src/app/animation-studio-folsom/layout.js
@@ -0,0 +1,25 @@
+//===== Meta Data =====
+export const metadata = {
+  //===== Meta Tags =====
+  title: "Top Animation Studio In Folsom  - Infinity Animations",
+  description:
+    "Get high-converting 3D product animation videos in Folsom. Infinity Animations turns product features into eye-catching motion visuals that drive results.",
+  //===== OG Tags =====
+  openGraph: {
+    title: "Top Animation Studio In Folsom  - Infinity Animations",
+    description:
+      "Get high-converting 3D product animation videos in Folsom. Infinity Animations turns product features into eye-catching motion visuals that drive results.",
+    url: "/animation-studio-folsom",
+    siteName: "Infinity Animations",
+    locale: "en_US",
+    type: "website",
+  },
+  //===== Canonical =====
+  alternates: { canonical: "/animation-studio-folsom" },
+  //===== GEO Tags =====
+  other: {},
+}
+
+export default function RootLayout({ children }) {
+  return children
+}
