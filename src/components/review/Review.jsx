b72328374b2aec2c3cde2c444e913866cfae2cd1
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2025-07-15 01:45:56 +0500

    new images with bgwhite

--- src/components/review/Review.jsx
+++ src/components/review/Review.jsx
@@ -26,12 +26,12 @@ import { AutoScroll } from '@/components/sliders';
 const Review = () => {
     return (
         <>
-            <section className="flex items-center justify-center sm:pt-3 sm:pb-2 md:mb-0 lg:pb-10 mt-[30px] sm:mt-[80px] md:mt-[50px] lg:mt-0">
-                <AutoScroll wrapperClass="mt-2 lg:mt-5 mb-10 lg:mb-0" options={{ loop: true, align: "start" }}>
+            <section className="bg-white flex items-center justify-center sm:pt-3 sm:pb-2 md:mb-0 lg:pb-3 mt-[30px] sm:mt-[80px] md:mt-[50px] lg:mt-0">
+                <AutoScroll wrapperClass=" mb-5 lg:mb-0" options={{ loop: true, align: "start" }}>
                     {
                         [icon1, icon2, icon3, icon4, icon5, icon6, icon7, icon8,icon9,icon10,icon11,icon12,icon13,icon14,icon15,icon16 ].map((e, i) => (
                             <div key={i} className="shrink-0 grow-0 basis-1/3 md:basis-1/6 mr-2">
-                                <Image src={e} alt="logo" className='w-[90%] sm:w-[70%]' />
+                                <Image src={e} alt="logo" className='w-[90%] sm:w-[80%]' />
                             </div>
                         ))
                     }
